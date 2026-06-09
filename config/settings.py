f8e43c23d5902aa0c32ee2fad6e2e06d2e315c3d
zunair092 <zunair.snexus@gmail.com>
2026-06-10 03:01:33 +0500

    chaneel layer update

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,10 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
-            "capacity": 1500,
-            "expiry": 10,
+            "socket_timeout": None,        # disable read timeout
+            "socket_connect_timeout": 5,   # still timeout on connect
         },
-    },
+    }
 }
 
 MIDDLEWARE = [
