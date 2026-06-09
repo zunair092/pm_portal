ea4c2cbd0df05e7853f1d0fe75de2d45f35d53b0
zunair092 <zunair.snexus@gmail.com>
2026-06-10 02:45:19 +0500

    routing.py

--- core/routing.py
+++ core/routing.py
@@ -1,13 +1,6 @@
-# from django.urls import path
-# from .consumers import NotificationConsumer
-
-# websocket_urlpatterns = [
-#     path('ws/notifications/', NotificationConsumer.as_asgi()),
-# ]
-
 from django.urls import re_path
-from . import consumers # Make sure you have a consumers.py
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'^ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
 ]
